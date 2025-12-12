84b7763fbaff6c4d679b579b84e36d5728e8850c
Ktoto198 <frybenderer43@gmail.com>
2025-12-12 14:57:42 +0100

    added russian language

--- django-app/django_app/urls.py
+++ django-app/django_app/urls.py
@@ -6,10 +6,12 @@ from datetime import datetime
 from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 from app import forms, views
 
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('', views.home, name='home'),
     path('favourites/', views.view_favorites, name='favourites'),
     path('edit-song/<int:songID>/', views.edit_Song, name='edit_song'),
@@ -17,7 +19,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('games/<int:gameID>/', views.game_detail, name='game_detail'),
     path('games/<int:gameID>/api/get-comments/', views.get_comments, name='get_comments'),
-#     path('login/', views.loginView, name='login'),
     path('accounts/login/', views.loginView, name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
@@ -25,5 +26,9 @@ urlpatterns = [
     path('api/add-to-favs/<int:songId>/', views.add_to_favs, name='add_to_favs'),
     path('api/get-favs-per-game/<int:gameId>/', views.get_favs_per_game, name='get_favs_per_game'),
     path('api/get-fav-songs/', views.get_fav_songs, name='get_fav_songs'),
-    path('api/search-games/', views.search_games, name = 'search_games'),
+    path('api/search-games/', views.search_games, name='search_games'),
+    prefix_default_language=False,
+) + [
+    path('set-language/', views.set_language_preference, name='set_language_preference'),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
